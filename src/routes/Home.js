499082bde2286128322ef237258bbdeba235f0b1
Ayomipo Majek <ayomipomajek@gmail.com>
2023-08-28 21:21:43 +0100

    home page modified

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Heroimg from '../components/Heroimg';
+import Myworks from '../components/Myworks';
+import Details from '../components/Details';
+import Form from '../components/Form';
 
 const Home = () => (
   <div>
     <Navbar />
     <Heroimg />
+    <Myworks />
+    <Details />
+    <Form />
   </div>
 );
 
